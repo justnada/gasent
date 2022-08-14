d15fb42e28ff88bbcaf3bae1cd1190e3db2d23c7
justnada <naddyyum@gmail.com>
2022-08-14 22:43:11 +0700

    feat: Add category notifications

--- gasent-server/app/category/controller.js
+++ gasent-server/app/category/controller.js
@@ -2,14 +2,17 @@ const Category = require("./model");
 
 const index = async (req, res) => {
   try {
+    const alertMsg = req.flash("alertMsg");
+    const alertStatus = req.flash("alertStatus");
+
+    const alert = { message: alertMsg, status: alertStatus };
     const categories = await Category.find();
 
-    console.log("CATEGORY VIEW");
-    console.log(categories);
-
-    res.render("admin/category/view_category", { categories: categories });
+    res.render("admin/category/view_category", { categories, alert });
   } catch (err) {
-    console.log(err);
+    req.flash("alertMsg", `${err?.message}`);
+    req.flash("alertStatus", "danger");
+    res.redirect("/category");
   }
 };
 
@@ -17,7 +20,9 @@ const createView = async (req, res) => {
   try {
     res.render("admin/category/create");
   } catch (err) {
-    console.log(err);
+    req.flash("alertMsg", `${err?.message}`);
+    req.flash("alertStatus", "danger");
+    res.redirect("/category");
   }
 };
 
@@ -28,9 +33,13 @@ const createAction = async (req, res) => {
     let category = new Category({ name });
     await category.save();
 
+    req.flash("alertMsg", `Category '${name}' created`);
+    req.flash("alertStatus", "success");
     res.redirect("/category");
   } catch (err) {
-    console.log(err);
+    req.flash("alertMsg", `${err?.message}`);
+    req.flash("alertStatus", "danger");
+    res.redirect("/category");
   }
 };
 
@@ -41,7 +50,9 @@ const editView = async (req, res) => {
 
     res.render("admin/category/edit", { category });
   } catch (err) {
-    console.log(err);
+    req.flash("alertMsg", `${err?.message}`);
+    req.flash("alertStatus", "danger");
+    res.redirect("/category");
   }
 };
 
@@ -50,24 +61,37 @@ const editAction = async (req, res) => {
     const { id } = req.params;
     const { name } = req.body;
 
-    const category = await Category.findOneAndUpdate({ _id: id }, { name });
-    console.log(category);
+    await Category.findOneAndUpdate({ _id: id }, { name })
+      .then(() => {
+        req.flash("alertMsg", `Category '${name}' updated`);
+        req.flash("alertStatus", "success");
+      })
+      .catch((err) => {
+        req.flash("alertMsg", `${err?.message}`);
+        req.flash("alertStatus", "danger");
+      });
+
     res.redirect("/category");
   } catch (err) {
-    console.log(err);
+    req.flash("alertMsg", `${err?.message}`);
+    req.flash("alertStatus", "danger");
+    res.redirect("/category");
   }
 };
 
 const deleteAction = async (req, res) => {
   try {
     const { id } = req.params;
-    await Category.findOneAndDelete({ _id: id });
+    const category = await Category.findOne({ _id: id });
+    await Category.deleteOne({ _id: id });
 
-    console.log("delete controller");
-
+    req.flash("alertMsg", `Category '${category?.name}' deleted`);
+    req.flash("alertStatus", "success");
     res.redirect("/category");
   } catch (err) {
-    console.log(err);
+    req.flash("alertMsg", `${err?.message}`);
+    req.flash("alertStatus", "danger");
+    res.redirect("/category");
   }
 };
 
